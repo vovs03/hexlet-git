89e01b2ae13775cbc916f590f651f67c05867f1a
Vladimir Victor Pavlychev <vladimir.keyjoo@gmail.com>
2023-03-29 11:53:33 +0300

    Fix: remove commentaries

--- lessons/07/flow.sh
+++ lessons/07/flow.sh
@@ -1,9 +1,8 @@
-# При выполнении задачи состояние репозитория могло измениться.
-# Перед набором команд из списка, обновите упражнение с помощью кнопки Сброс
-
-cd code-user/ # Переходим в директорию code-user
+cd code-user/
 git status
+
 git restore hexlet.txt
 git restore --staged index.md index.html
 git restore index.html
+
 git clean -fd
